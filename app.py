080efa51dc29bf38512a0afe50130864beb03ab6
Yash Chaurasia <yashchaurasia525yc@gmail.com>
2026-04-23 11:42:22 +0000

    fix model

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 @app.route("/jarvis", methods=["POST"])
 def jarvis():
